d6721989b454f39f6db0918d9f79c8eb65062c78
= <oliverasmith123@gmail.com>
2025-02-14 23:36:20 +0000

    Improved logo

--- js/logo.js
+++ js/logo.js
@@ -4,6 +4,7 @@
 const _graphic = (function() {
     let state = 0;
     // 0 = loading | 1  = ready to be played | 2 = has been played
+    let widths = [];
 
     (function () { // Initialiser
         window['play'] = play;
@@ -14,7 +15,6 @@ const _graphic = (function() {
         
         const graphic = document.querySelector('.lt-style-one .graphic');
         const lines = graphic.textContent.split('\n').slice(1,-1);
-        console.log(lines)
         graphic.textContent = ""
         let template = document.createElement("div")
         template.className = "text-line"
@@ -25,8 +25,20 @@ const _graphic = (function() {
             // else if (i==3)  line_elem.style.setProperty('padding-left', '3vw')
             // else if (i==4)  line_elem.style.setProperty('padding-left', '7vw')
             line_elem.appendChild(document.createTextNode(line))
+            
             graphic.appendChild(line_elem)
         } )
+        const [...linesElem]   = document.querySelectorAll('.text-line')
+        linesElem.forEach((line, i) => {
+            widths.push(line.offsetWidth)
+        })
+        new gsap.set(linesElem, {
+            width: 0
+        })
+        new gsap.set(linesElem, {
+            delay: 0.1,
+            opacity: 1
+        })
         state = 1
     }
     function play() {
@@ -34,35 +46,80 @@ const _graphic = (function() {
             console.log("animating...")
             // Play Animation
             animateIn()
+            state=2
         } else if (state == 2) {
             // Hide Animation
-
+            animateOut()
+            state=1
         }
     }
     function animateIn() {
         return new Promise((resolve, reject) => {
             // const graphic = document.querySelector('.lt-style-one .graphic');
             const [...lines]   = document.querySelectorAll('.text-line')
-            const t1  = new gsap.timeline({duration: 5, ease: 'power1.in', onComplete: resolve});    
+            const t1  = new gsap.timeline({duration: 1.5, ease: 'power1.in', onComplete: resolve});    
             
+            lines.forEach((line, i) => {
+                t1.to(line, {
+                    width: widths[i], duration: 1.5,
+                    paddingLeft: '1vw',
+                    paddingRight: '2vw',
+                }, 0)
+            })
+            // }, 0)
+            // t1.set(lines, {
+            //     'margin-right': '100%',
+            //     overflow: 'hidden'
+            // }, 0)
+            // .to(
+            //     lines, {
+            //         duration: 1,
+            //         'margin-right': '0%',
+            //         stagger: {
+            //             amount: 0.2
+            //         },
+            //     }
+            // )
+            // .to(lines, {
+            //     delay: 1,
+            //     paddingLeft: '1vw',
+            //     paddingRight: '2vw',
+            //     stagger: {
+            //         amount: 0.2
+            //     },
+            //     ease: "power1.inOut"
+            // }, 0)
+            // t1.to(lines, {opacity: 1}, 0)
+        })
+    }
+    function animateOut() {
+        return new Promise((resolve, reject) => {
+            const [...lines]   = document.querySelectorAll('.text-line')
+            const t1  = new gsap.timeline({ease: 'power1.in', onComplete: resolve});    
             
-            t1.from(lines, {
-                delay: 1, width: 0, duration: 0.5,
+            t1.to(lines, {
+                width: 0, duration: 1,
+                paddingLeft: '0',
+                paddingRight: '0',
                 stagger: {
                     amount: 0.2
-                },
-                ease: "power1.inOut"
-            }, 0)
-            .to(lines, {
-                delay: 1,
-                paddingLeft: '1vw',
-                paddingRight: '2vw',
-                stagger: {
-                    amount: 0.2
-                },
-                ease: "power1.inOut"
-            }, 0)
-            .to(lines, {opacity: 1}, 0)
+                }})
+            // t1.to(lines, {
+            //     delay: 1, width: 0, duration: 0.5,
+            //     stagger: {
+            //         amount: 0.2
+            //     },
+            //     ease: "power1.inOut"
+            // }, 0)
+            // .to(lines, {
+            //     delay: 1,
+            //     paddingLeft: '0',
+            //     paddingRight: '0',
+            //     stagger: {
+            //         amount: 0.2
+            //     },
+            //     ease: "power1.inOut"
+            // }, 0)
         })
     }
 
