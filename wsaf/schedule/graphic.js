d422202d3ff546f05bdedc784e04025fcbcdf885
Oliver Smith <oliver.smith.6@warwick.ac.uk>
2025-06-10 15:48:51 +0100

    Schedule displaying data properly

--- wsaf/schedule/graphic.js
+++ wsaf/schedule/graphic.js
@@ -1,9 +1,9 @@
-const PRETALX_BASE_URL = 'http://localhost:8010/proxy'//'https://submit.wsaf.org.uk';
-// Need to setup a cors proxy using 'npm run proxy'
+const PRETALX_BASE_URL = 'http://localhost:8010/proxy';  // <- Need to setup a cors proxy using 'npm run proxy'
+// const PRETALX_BASE_URL = 'https://submit.wsaf.org.uk';  // <- Waiting for Adam to add CORS to server
+
 const PRETALX_EVENT_SLUG = '2025';
 const PRETALX_API_TOKEN = 'l4jfvarm5wg24bgcxwj584e0c110jfms68qb8288cxfpt445362huaif7n5gmf72'
-const WSAF_BASE_URL = 'http://localhost:4000/api'
-// Run the wsaf website locally (or use the uwcs dev version) - until the CORS change is pushed to prod
+const WSAF_BASE_URL = 'https://wsaf.org.uk/api'
 const EVENTS_STAGGER = 0.2
 let state = 0;
 let event_num = 0;
@@ -52,40 +52,86 @@ Object.filter = (obj, predicate) =>
     Object.keys(obj)
           .filter( key => predicate(key, obj[key]) )
           .reduce( (res, key) => (res[key] = obj[key], res), {} );
+Object.filterV = (obj, predicate) => 
+    Object.keys(obj)
+          .filter( key => predicate(key, obj[key]) )
+          .reduce( (res, key) => (res.push(obj[key]), res), [] );
+
 
 async function updateSchedule(filters) {
-    console.log(filters)
     const result = await fetchSchedule();
     const info = result.schedule.conference;
-    console.log(info)
     const tracks = info.tracks;
-    const events = info.days.filter(d => filters.start ? !(Date.parse(d.day_end) < filters.start || Date.parse(d.day_start)> filters.end): true).flatMap((day)=> 
-        Object.values(Object.filter(day.rooms, (k,v) => filters.rooms ? filters.rooms.includes(k) : true))
-    )
-    events.forEach(async event => {
-        
-    });
-    console.log(events)
+    console.log(info.days)
+    const days = info.days.filter(d => filters.start ? !(new Date(d.day_end) < filters.start || new Date(d.day_start)> filters.end): true).reduce((res, day)=>{
+        let filtered_rooms = Object.filterV(day.rooms, (k,v) => filters.rooms ? filters.rooms.includes(k) : true).flat(1);
+        if (filtered_rooms.length > 0)
+            res[(typeof day.index == 'number') ? day.date : day.index] = filtered_rooms;
+        return res;
+    },{})
+    console.log(days)
+    for(const [day,events] of Object.entries(days)) {
+        console.log(events.sort((a,b)=>Date.parse(a.date)>Date.parse(b.date)))
+        Promise.all(events.sort((a,b)=>Date.parse(a.date)>Date.parse(b.date)).map(async event => {
+            /* code, do_not_record:bool, date */
+            const event_d = await fetchEventDetails(event.code);
+            /*  artGallery: bool, artist: { name, description, image, instagramHandle, website },
+                categoryPretalxTrack, description, dropIn: bool, id, image, name, sessionCount: number,
+                sessions: [{ durationCategory, start, end, id, minutes, venue }], shortDescription, slug */
+            let event_date = new Date(event.date).toISOString();
+            const session_d = event_d.sessions.filter(s => s.start == event_date)[0];
+            return newEvent(event_d, session_d);
+        })).then(async (elems) => {
+            const parent = document.getElementById('events');
+            for(const elem of elems) {
+                await parent.appendChild(elem);
+            }
+        })
+    }
 }
 
 
-function newEvent(eventInfo) {
-    console.log("Woooo");
+function newEvent(eventInfo, sessionInfo) {
+    /*eventInfo
+        artGallery: bool, artist: { name, description, image, instagramHandle, website },
+        categoryPretalxTrack, description, dropIn: bool, id, image, name, sessionCount: number,
+        sessions: [{ durationCategory, start, end, id, minutes, venue }], shortDescription, slug
+      sessionInfo
+        durationCategory, start, end, id, minutes, venue
+    */
     const template = document.getElementById('event-template');
     let cloned = template.content.cloneNode(true);
     let event = cloned.getElementById("event")
-    const parent = document.getElementById('events');
     
-    event.id='event-'+(event_num++);
+    
+    event.id='event-'+event.id;
 
-    event.querySelector('#event-time').textContent = eventInfo.time || '';
+    event.querySelector('#event-time').textContent = (sessionInfo.start && sessionInfo.end) ? formatTimeStr(sessionInfo.start, sessionInfo.end) : '';
     event.querySelector('#event-name').textContent = eventInfo.name || '';
-    event.querySelector('#event-by').textContent = eventInfo.by || '';
+    event.querySelector('#event-by').textContent = eventInfo.artist.name;
     
-
-    parent.appendChild(cloned);
-    events[eventInfo.id] = document.getElementById(event.id);
+    return cloned;
+    // parent.appendChild(cloned);
+    // events[eventInfo.id] = event.id;
 }
+function formatTimeStr(start_s, end_s) {
+    const start = new Date(start_s);
+    const end   = new Date(end_s);
+    let options = {
+        "hour": "numeric",
+        "minute": "2-digit",
+        "hour12": true
+    };
+    if(start.getDate() != end.getDate()) {
+        options["weekday"] ="short";
+    }
+    let start_f = start.toLocaleString([], options);
+    let end_f = end.toLocaleString([], options)
+    if(!(start.getHours() < 12 && end.getHours() >= 12)) {
+        start_f = start_f.slice(0, -2);
+    }
+    return start_f + " - " + end_f;
+} 
 function animateInEvent(t_l, event, start) {
     console.log("Animating")
     const event_time = event.querySelector('#event-time')
@@ -122,9 +168,9 @@ function animateOut() {
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    fetchEventDetails("8C9P73").then(result => console.log(result));
-    // updateSchedule({start: Date.parse('2025-06-13T04:00:00+01:00'), end: Date.parse('2025-06-14T03:59:00+01:00'), rooms: ['Benefactors Place Stage']})
-    newEvent({time: '12-3pm', name: 'test', type: 'music', id: '123'})
-    newEvent({time: '12-3pm', name: 'test', type: 'music', id: '124'})
-    newEvent({time: '12-3pm', name: 'test', type: 'music', id: '125'})
+    // fetchEventDetails("8C9P73").then(result => console.log(result));
+    updateSchedule({start: new Date('2025-06-13T04:00:00+01:00'), end: new Date('2025-06-14T03:59:00+01:00'), rooms: ['Benefactors Place Stage']})
+    // newEvent({time: '12-3pm', name: 'test', type: 'music', id: '123'})
+    // newEvent({time: '12-3pm', name: 'test', type: 'music', id: '124'})
+    // newEvent({time: '12-3pm', name: 'test', type: 'music', id: '125'})
 })
