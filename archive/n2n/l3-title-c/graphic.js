beefcfab4921dc46124bddaff34e07d5739cbb1c
Oliver Smith <oliver.smith.6@warwick.ac.uk>
2025-11-16 15:50:37 +0000

    Moved projects to an archive folder

---
+++ archive/n2n/l3-title-c/graphic.js
@@ -0,0 +1,36 @@
+gsap.registerPlugin(SplitText) 
+
+// function g_updateS(graphicS, changes) {
+// }
+// function g_update(changes, n_height, n_width) {
+// }
+
+
+function g_animateIn(tl) {
+    // let title = document.querySelector('.title').textContent;
+    let split = SplitText.create(".title", { type: "words, lines" });
+    tl.set('.title', {
+        opacity: 1,
+        text: ""
+    })
+    // tl.to('.title', {
+    //     duration: 2,
+    //     text: {
+    //         value: title,
+    //         delimiter: " "
+    //     },
+    // })
+    tl.from(split.words, {
+        duration: 1, 
+        y: 100,       // animate from 100px below
+        autoAlpha: 0, // fade in from opacity: 0 and visibility: hidden
+        stagger: 0.05 // 0.05 seconds between each
+    })
+    tl.from('.sub', {
+        opacity: 0,
+        y: 20
+    })
+}
+function g_animateOut(tl) {
+
+}
